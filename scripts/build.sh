0c67e0e14645e527d97938b22fc8ac5c68eb6a05
E.J. Finneran <ej.finneran@gmail.com>
2021-03-08 23:04:43 -0800

    Test

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,8 @@ export ELM_VERSION=0.19.1
 export JS=bingo.js 
 export JSMIN=build/bingo.min.js
 
-npm install -g elm@$ELM_VERSION
-npm install -g uglify-js
+npm install elm@$ELM_VERSION
+npm install uglify-js
 
 elm make src/Main.elm --optimize --output=$JS
 uglifyjs $JS --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > $JSMIN
