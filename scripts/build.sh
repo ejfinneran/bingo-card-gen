dd42a4c0a60e924ea882c7b773686c97c16c862d
E.J. Finneran <ej.finneran@gmail.com>
2021-03-08 23:03:50 -0800

    Build script

---
+++ scripts/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+mkdir -p build
+export ELM_VERSION=0.19.1
+export JS=bingo.js 
+export JSMIN=build/bingo.min.js
+
+npm install -g elm@$ELM_VERSION
+npm install -g uglify-js
+
+elm make src/Main.elm --optimize --output=$JS
+uglifyjs $JS --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle > $JSMIN
+
+cp config.json d3plus-text.full.min.js index.html offline.js saveSvgAsPng.js style.css build/
